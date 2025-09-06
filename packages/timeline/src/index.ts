e8e5248253a6a824f32874310da7c46334e0a605
oyerindedaniel <oyerinde.daniel@yahoo.com>
2025-09-07 00:23:10 +0100

    cleanup timeline core

--- packages/timeline/src/index.ts
+++ packages/timeline/src/index.ts
@@ -1,21 +1,8 @@
-// Main Timeline component and compound components
-export { Timeline } from "./timeline";
-
 // Context and provider
 export { TimelineRootProvider, useTimelineContext } from "./context";
 
 // Hooks
 export { useVideoThumbnails } from "./timeline-hooks/use-video-thumbnails";
 
-export type {
-  TimelineItem,
-  TimelineProps,
-  TimelineItemProps,
-  TimelineContentProps,
-  TimelineHeaderProps,
-  TimelineDescriptionProps,
-  TimelineDateProps,
-  TimelineConnectorProps,
-  TimelineContextValue,
-  TimelineComponent,
-} from "./utils/types";
+// Timeline component/types
+export * from "./timeline";
